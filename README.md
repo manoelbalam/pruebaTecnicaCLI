9796530b465685ee9c7c55fa4be634dd24f50268
Thare Valverde <thare.valverde@mxtech.group>
2022-07-15 11:46:20 -0500

    cli stable

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# pruebaTecnicaCLI - init repo
+# Prueba técnica - Desarrollador Python - Componente 2
+###  Client line interface created with click
+
+## Installation
+After clone repo.
+```sh
+$ cd into..
+$ python3 -m venv env
+$ source env/bin/activate
+(env) $ pip install -r requirements.txt
+(env) $ python cli.py
+```
+## Usage
+```sh
+(env) $ python cli.py --help
+Usage: cli.py [OPTIONS]
+
+Options:
+  -b, --backend TEXT             Backend address ex. localhost:8000 [required]
+  -e, --endpoint [signup|login]  [required]
+  -u, --useraccess TEXT          UserAccess
+  -p, --passaccess TEXT          PassAccess
+  -d, --date TEXT                date format YYYY-MM-DD [optional]
+  --help                         Show this message and exit.
+```
--- cli.py
+++ cli.py
@@ -1,8 +1,28 @@
-import requests
+#!/usr/bin/python3
 
-URL = "http://127.0.0.1:8000/api/Seguridad/signup/"
-URLogin = "http://127.0.0.1:8000/api/Seguridad/login/"
-PARAMS = {"UserAccess":"admin","PassAccess":"admin"}
-r = requests.post(url = URL, json = PARAMS)
-data = r.json()
-print(data)
+import click, requests
+
+@click.command()
+@click.option('-b','--backend', prompt=True, required=True, help='Backend address ex. localhost:8000')
+@click.option('-e','--endpoint', prompt=True, required=True, type=click.Choice(['signup', 'login'], case_sensitive=False))
+@click.option('-u','--useraccess', prompt=True, help='UserAccess')
+@click.option('-p','--passaccess', prompt=True, hide_input=True,confirmation_prompt=True, help='PassAccess')
+@click.option('-d','--date', prompt=False, type=str,help='date format YYYY-MM-DD [optional]')
+
+def ptCLI(backend,endpoint,useraccess,passaccess,date):
+
+    if endpoint == 'signup':
+        url = 'http://' + backend + '/api/Seguridad/' + endpoint + '/'
+        if not useraccess or not passaccess:
+            res = requests.get(url = url)
+            click.echo(f'{res.json()}')
+        res = requests.post(url = url, json = {'UserAccess':useraccess,'PassAccess':passaccess, 'ExpirationAccess':date})
+        click.echo(f'{res.json()}')
+
+    if endpoint == 'login':
+        url = 'http://' + backend + '/api/Seguridad/' + endpoint + '/'
+        res = requests.post(url = url, json = {'UserAccess':useraccess,'PassAccess':passaccess})
+        click.echo(f'{res.json()}')
+
+if __name__ == '__main__':
+    ptCLI()
