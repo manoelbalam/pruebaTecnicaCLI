766ba001f9a400625b7d8657a0069daa4589a982
Thare Valverde <thare.valverde@mxtech.group>
2022-08-11 14:07:50 -0500

    fix get method

--- cli.py
+++ cli.py
@@ -5,8 +5,8 @@ import click, requests
 @click.command()
 @click.option('-b','--backend', prompt=True, required=True, help='Backend address ex. localhost:8000')
 @click.option('-e','--endpoint', prompt=True, required=True, type=click.Choice(['signup', 'login'], case_sensitive=False))
-@click.option('-u','--useraccess', prompt=True, help='UserAccess')
-@click.option('-p','--passaccess', prompt=True, hide_input=True,confirmation_prompt=True, help='PassAccess')
+@click.option('-u','--useraccess', prompt=False, help='UserAccess')
+@click.option('-p','--passaccess', prompt=False, hide_input=True,confirmation_prompt=True, help='PassAccess')
 @click.option('-d','--date', prompt=False, type=str,help='date format YYYY-MM-DD [optional]')
 
 def ptCLI(backend,endpoint,useraccess,passaccess,date):
@@ -16,8 +16,9 @@ def ptCLI(backend,endpoint,useraccess,passaccess,date):
         if not useraccess or not passaccess:
             res = requests.get(url = url)
             click.echo(f'{res.json()}')
-        res = requests.post(url = url, json = {'UserAccess':useraccess,'PassAccess':passaccess, 'ExpirationAccess':date})
-        click.echo(f'{res.json()}')
+        else:
+            res = requests.post(url = url, json = {'UserAccess':useraccess,'PassAccess':passaccess, 'ExpirationAccess':date})
+            click.echo(f'{res.json()}')
 
     if endpoint == 'login':
         url = 'http://' + backend + '/api/Seguridad/' + endpoint + '/'
